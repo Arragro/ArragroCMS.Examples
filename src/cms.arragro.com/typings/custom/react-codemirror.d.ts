446aaba0cce952364634b35377563fd6b5060416
Michael Jewell <michael_jewell@hotmail.co.uk>
2017-09-13 12:15:33 +1200

    Added typings

---
+++ src/cms.arragro.com/typings/custom/react-codemirror.d.ts
@@ -0,0 +1,5 @@
+﻿import * as React from 'react';
+
+declare module "react-codemirror" {
+    export default class CodeMirror extends React.Component <any, any> {}
+}
---
+++ src/cms.arragro.com/typings/custom/react-copy-to-clipboard.d.ts
@@ -0,0 +1,5 @@
+﻿import * as React from 'react';
+
+declare module "react-copy-to-clipboard" {
+    export default class CopyToClipboard extends React.Component<any, any> { }
+}
