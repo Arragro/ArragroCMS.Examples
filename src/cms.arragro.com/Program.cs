873a6d7af0a0d33971fef04cce7a5a1de8ee38ff
Michael Jewell <michael_jewell@hotmail.co.uk>
2017-10-03 08:16:42 +1300

    Removed AssemblyName from csproj, and implemented 2.0 standards to Program.cs

--- src/cms.arragro.com/Program.cs
+++ src/cms.arragro.com/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore;
 
 namespace cms.arragro.com
 {
@@ -11,15 +7,12 @@ namespace cms.arragro.com
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseApplicationInsights()
-                .Build();
-
-            host.Run();
+            BuildWebHost(args).Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
     }
 }
