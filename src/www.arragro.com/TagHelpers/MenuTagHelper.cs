2063f875bfd671eeca1eed1f4ec6adf15b080720
Michael Jewell <michael_jewell@hotmail.co.uk>
2017-11-16 17:17:02 +1300

    Layout now uses new menu taghelpers and has footer

---
+++ src/www.arragro.com/TagHelpers/MenuTagHelper.cs
@@ -0,0 +1,63 @@
+﻿using ArragroCMS.Core.Web.DataTransformationObjects;
+using ArragroCMS.Web.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace www.arragro.com.TagHelpers
+{
+    public class MenuTagHelper : TagHelper
+    {
+        private readonly IArragroCmsManagementClient _arragroCmsManagementClient;
+
+        public MenuTagHelper(
+            IArragroCmsManagementClient arragroCmsManagementClient)
+        {
+            _arragroCmsManagementClient = arragroCmsManagementClient;
+        }
+        protected HttpRequest Request => ViewContext.HttpContext.Request;
+        protected HttpResponse Response => ViewContext.HttpContext.Response;
+        
+        public ViewContext ViewContext { get; set; }
+
+        private string GetDropdownItems(string id, IEnumerable<SiteMapDto> sitemaps)
+        {
+            var output = new StringBuilder($"<div class=\"dropdown-menu\" aria-labelledby=\"{id}\">");
+
+            foreach (var sitemap in sitemaps)
+            {
+                output.Append($"<a class=\"dropdown-item\" href=\"/{sitemap.Url}\">{sitemap.Name}</a>");
+            }
+
+            output.Append("</div>");
+
+            return output.ToString();
+        }
+
+        private string GetNavItems(SiteMapContainerDto sitemapContainer)
+        {
+            var output = new StringBuilder();
+            foreach (var sitemap in sitemapContainer.SiteMaps)
+            {
+                output.Append($"<li class=\"nav-item\"><a href=\"/{sitemap.Url}\" class=\"nav-link\">{sitemap.Name}</a></li>");
+            }
+            return output.ToString();
+        }
+        
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var sitemapContainer = await _arragroCmsManagementClient.GetSitemapAsync();
+
+            output.TagName = "ul";
+            output.Attributes.Add(new TagHelperAttribute("class", "navbar-nav"));
+            output.Content.SetHtmlContent($"{GetNavItems(sitemapContainer)}");
+            output.TagMode = TagMode.StartTagAndEndTag;
+        }
+    }
+}
---
+++ src/www.arragro.com/TagHelpers/MetadataTagHelpers.cs
@@ -0,0 +1,29 @@
+﻿using ArragroCMS.Core.Web.DataTransformationObjects;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace www.arragro.com.TagHelpers
+{
+    public class MetadataTagHelper : TagHelper
+    {
+        public string SiteName { get; set; }
+        public string Url { get; set; }
+        public ContentDtoJsonAsObject Content { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            var title = string.IsNullOrEmpty(Content.Title) ? "Arragro Software Developement for the Cloud" : $"{Content.Title} - Arragro";
+            var description = string.IsNullOrEmpty(Content.Description) ? "Azure Architecture Deployment .Net ASP.Net MVC" : Content.Description;
+            var keywords = string.IsNullOrEmpty(Content.Keywords) ? "Azure Architecture Deployment .Net ASP.Net MVC" : Content.Keywords;
+
+            output.TagName = "";
+            output.Content.SetHtmlContent($@"
+      <title>{title}</title>
+      <meta name=""description"" content=""{description}"" />
+      <meta name=""keywords"" content=""{keywords}"" />
+      <meta property=""og:title"" content=""{title}"">
+      <meta property=""og:site_name"" content=""{SiteName}"" />
+      <meta property=""og:url"" content=""{Url}"" />
+      <meta property=""og:description"" content=""{description}"" />");
+        }
+    }
+}
