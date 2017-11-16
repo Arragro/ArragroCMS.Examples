9683dce8f558ade4789175be3e521f4291652934
Michael Jewell <michael_jewell@hotmail.co.uk>
2017-11-16 18:02:21 +1300

    Added new interfaces

--- src/cms.arragro.com/ReactApp/CustomComponents/interfaces.ts
+++ src/cms.arragro.com/ReactApp/CustomComponents/interfaces.ts
@@ -7,14 +7,31 @@ export interface ICarousel extends IItemType {
     href: string
 }
 
+export interface ICloud extends IItemType {
+    name: string
+    svgBased: boolean
+    svgIcon: string
+    imageUrl: string
+    imageAlt: string
+    markdown: string
+    hasLink: boolean
+    linkText: string
+    href: string
+}
+
 export interface IRandomCarousel {
     carousels: Array<ICarousel>
+}
+
+export interface ICloudBannerText {
+    markdown: string
 }
 
 export interface ISvgIconLink {
     title: string
     svg: string
     href: string
+    markdown: string
 }
 
 export interface IItemType {
