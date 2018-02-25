111e06f765e9cf6b62efdffc0fde8b7a65d1eb48
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-02-25 19:19:04 +1300

    Added Tile, and made cloud inherit from it

---
+++ src/arragro.com.ContentTypes/Models/Tile.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace arragro.com.ContentTypes.Models
+{
+    public class Tile
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        public bool SvgBased { get; set; }
+
+        [MaxLength(2000)]
+        public string ImageUrl { get; set; }
+        [MaxLength(512)]
+        public string ImageUrlAlt { get; set; }
+
+        public bool HasLink { get; set; }
+        [MaxLength(100)]
+        public string LinkText { get; set; }
+        [MaxLength(2000)]
+        public string Href { get; set; }
+        [MaxLength(4000)]
+        public string Markdown { get; set; }
+        [MaxLength(30)]
+        public string CssClass { get; set; }
+
+    }
+}
