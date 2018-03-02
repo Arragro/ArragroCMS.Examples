354981778c19d20ae666b0f6c4952ac8ca3a3a49
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-03-03 11:53:34 +1300

    Upgraded nuget packages, and added ContactForm to arragro.com.ContentTypes

---
+++ src/arragro.com.ContentTypes/Models/ContactForm.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
+
+namespace arragro.com.ContentTypes.Models
+{
+    public class ContactForm
+    {
+        [Required]
+        [JsonIgnore]
+        public string RecapchtaResponse { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string LastName { get; set; }
+        [MaxLength(100)]
+        public string Company { get; set; }
+        [Required]
+        [RegularExpression(@"^(\+\s?)?((?<!\+.*)\(\+?\d+([\s\-\.]?\d+)?\)|\d+)([\s\-\.]?(\(\d+([\s\-\.]?\d+)?\)|\d+))*(\s?(x|ext\.?)\s?\d+)?$", ErrorMessage = "Please supply a valid phone number")]
+        public string Phone { get; set; }
+        [Required]
+        [MaxLength(2000)]
+        [EmailAddress]
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
