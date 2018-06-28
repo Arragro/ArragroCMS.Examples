8a6b19e5da8f1381adc3d505df3ca85939255dc7
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-06-28 20:54:43 +1200

    Log errors

--- src/www.arragro.com/Controllers/ErrorController.cs
+++ src/www.arragro.com/Controllers/ErrorController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using www.arragro.com.Models;
 
 namespace www.arragro.com.Controllers
@@ -8,6 +9,13 @@ namespace www.arragro.com.Controllers
     {
         private readonly MetaData _metaData404 = new MetaData("Not Found", "Content couldn't be found", "404 Not Found");
         private readonly MetaData _metaDataError = new MetaData("Error", "Something has gone wrong and we are investigating", "Error");
+
+        private readonly ILogger<ErrorController> _logger;
+
+        public ErrorController(ILogger<ErrorController> logger)
+        {
+            _logger = logger;
+        }
 
         [Route("error/{statusCode}")]
         public IActionResult Index(string statusCode)
@@ -26,21 +34,22 @@ namespace www.arragro.com.Controllers
         [Route("error/exception")]
         public IActionResult Exception()
         {
-            //var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+            var exceptionFeature = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
 
-            //if (exceptionFeature != null)
-            //{
-            //    // Get which route the exception occurred at
-            //    string routeWhereExceptionOccurred = exceptionFeature.Path;
+            if (exceptionFeature != null)
+            {
+                // Get which route the exception occurred at
+                string routeWhereExceptionOccurred = exceptionFeature.Path;
 
-            //    // Get the exception that occurred
-            //    var exceptionThatOccurred = exceptionFeature.Error;
+                // Get the exception that occurred
+                var exceptionThatOccurred = exceptionFeature.Error;
 
-            //    // TODO: Do something with the exception
-            //    // Log it with Serilog?
-            //    // Send an e-mail, text, fax, or carrier pidgeon?  Maybe all of the above?
-            //    // Whatever you do, be careful to catch any exceptions, otherwise you'll end up with a blank page and throwing a 500
-            //}
+                // TODO: Do something with the exception
+                // Log it with Serilog?
+                // Send an e-mail, text, fax, or carrier pidgeon?  Maybe all of the above?
+                // Whatever you do, be careful to catch any exceptions, otherwise you'll end up with a blank page and throwing a 500
+                _logger.LogError(exceptionThatOccurred, "Something went wrong!");
+            }
             ViewBag.MetaData = _metaDataError;
             return View("Index", ErrorPage.BuildError());
         }
