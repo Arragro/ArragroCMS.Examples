9cf932159e3cef7b37abfe6fc1e613d49cf3cf2c
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-07-07 15:33:55 +1200

    Added batch file for docker builds

---
+++ src/build.bat
@@ -0,0 +1,9 @@
+SET VERSION=v0.0.3-alpha
+docker build -f .\CMS.DockerFile -t docker.arragro.com/cms.arragro.com:%VERSION% . --no-cache
+docker build -f .\WWW.DockerFile -t docker.arragro.com/www.arragro.com:%VERSION% . --no-cache
+
+REM docker login docker.arragro.com
+REM docker push docker.arragro.com/cms.arragro.com:%VERSION%
+REM docker push docker.arragro.com/www.arragro.com:%VERSION%
+
+docker-compose up -d
