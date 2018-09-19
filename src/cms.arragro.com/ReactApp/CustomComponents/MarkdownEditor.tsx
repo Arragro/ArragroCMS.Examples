011d431526181c3210215cbf62f6a14dbdfdbfc5
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-09-19 14:54:46 +1200

    Implemented markdown editor from core app

--- src/cms.arragro.com/ReactApp/CustomComponents/MarkdownEditor.tsx
+++ src/cms.arragro.com/ReactApp/CustomComponents/MarkdownEditor.tsx
@@ -1,6 +1,6 @@
 ﻿import * as React from 'react'
 import * as ReactMarkdown from 'react-markdown'
-import { FormHelperText, Grid, Button, createStyles, Theme, WithStyles, withStyles } from '@material-ui/core'
+import { FormHelperText, Grid, Button, createStyles, Theme, WithStyles, withStyles, FormControl } from '@material-ui/core'
 import { Controlled as CodeMirror } from 'react-codemirror2'
 import { Interfaces, Components } from 'arragrocms-management'
 
@@ -8,8 +8,14 @@ import 'codemirror/lib/codemirror.css'
 import 'codemirror/theme/eclipse.css'
 import 'codemirror/mode/markdown/markdown'
 import 'codemirror/addon/display/autorefresh'
+import { FormControlProps } from '@material-ui/core/FormControl'
 
 const style = (theme: Theme) => createStyles({
+    formControl: {
+        width: '100%',
+        marginTop: theme.spacing.unit,
+        marginBottom: theme.spacing.unit
+    },
     preview: {
         paddingTop: 20,
         paddingLeft: 5,
@@ -28,6 +34,7 @@ export interface IMarkdownEditorProps extends WithStyles<typeof style> {
     submitCount: number
     showAssetPicker: boolean
     options?: any
+    formControlProps?: FormControlProps
     handleBlur (event: any): void
     setFieldValue (name: string, value: any): void
     saveStashedIncomplete? (): void
@@ -104,47 +111,46 @@ class MarkdownEditor extends React.Component<IMarkdownEditorProps, IMarkdownEdit
             autoRefresh: true
         }
 
-        const { contentData, classes } = this.props
-        const styleIssue = { border: '1px solid red' }
+        const { contentData, classes, formControlProps, submitCount, error } = this.props
+        const { dirty } = this.state
+        const styleIssue = { color: 'rgb(244, 67, 54)' }
         const style = (this.props.error !== undefined ? styleIssue : {})
         const mimeTypeFilter = 'image/jpeg,image/png,image/pjpeg,image/gif'
         const dropzoneAccept = 'image/jpeg,image/png,image/pjpeg,image/gif'
 
-        return <Grid container>
-            <Grid item xs={12} xl={8}>
-                <Grid container>
-                    <Grid item xs={12}>
-                        <label>{this.props.label}</label>
-                    </Grid>
-                    <Grid item xs={12} md={6} style={style}>
-                        <Components.AssetModal
-                            contentData={contentData}
-                            mimeTypeFilter={mimeTypeFilter}
-                            selectClick={this.selectClick}
-                            closeClick={this.closeClick}
-                            dropzoneAccept={dropzoneAccept}
-                            maxSize={10485760}
-                            showResize={true}
-                            saveStashedIncomplete={this.props.saveStashedIncomplete}
-                            modalOpen={this.state.assetModalOpen}
-                        />
-                        <Button onClick={this.showImageAssetModal} variant='contained' color='secondary' fullWidth={true}>Select Image</Button>
-                        {/* {utils.HtmlHelper.renderCmsButton(this.showImageAssetModal, null, 'Select Image', this.props.showAssetPicker, false)} */}
-                        <CodeMirror
-                            ref={(x) => this.codeMirror = x}
-                            value={this.props.value}
-                            options={options}
-                            onChange={this.onChange}
-                            onBeforeChange={this.onChange}
-                        />
-                        {this.getError()}
-                    </Grid>
-                    <Grid item xs={12} md={6} className={classes.preview} >
-                        <ReactMarkdown source={this.props.value} escapeHtml={false} />
-                    </Grid>
+        return <FormControl {...formControlProps} className={classes.formControl} error={(submitCount > 0 || dirty) && error !== undefined}>
+            <Grid container>
+                <Grid item xs={12}>
+                    <label style={style}>{this.props.label}</label>
+                </Grid>
+                <Grid item xs={12} md={6}>
+                    <Components.AssetModal
+                        contentData={contentData}
+                        mimeTypeFilter={mimeTypeFilter}
+                        selectClick={this.selectClick}
+                        closeClick={this.closeClick}
+                        dropzoneAccept={dropzoneAccept}
+                        maxSize={10485760}
+                        showResize={true}
+                        saveStashedIncomplete={this.props.saveStashedIncomplete}
+                        modalOpen={this.state.assetModalOpen}
+                    />
+                    <Button onClick={this.showImageAssetModal} variant='contained' color='secondary' fullWidth={true}>Select Image</Button>
+                    {/* {utils.HtmlHelper.renderCmsButton(this.showImageAssetModal, null, 'Select Image', this.props.showAssetPicker, false)} */}
+                    <CodeMirror
+                        ref={(x) => this.codeMirror = x}
+                        value={this.props.value}
+                        options={options}
+                        onChange={this.onChange}
+                        onBeforeChange={this.onChange}
+                    />
+                    {this.getError()}
+                </Grid>
+                <Grid item xs={12} md={6} className={classes.preview} >
+                    <ReactMarkdown source={this.props.value} escapeHtml={false} />
                 </Grid>
             </Grid>
-        </Grid>
+        </FormControl>
     }
 }
 
