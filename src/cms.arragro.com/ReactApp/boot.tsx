0c31fb1d7f938f45295aa6d35951ab8cc70cdc7c
Michael Jewell <michael_jewell@hotmail.co.uk>
2018-12-10 11:29:36 +1300

    Fixed base theme for material on the react app

--- src/cms.arragro.com/ReactApp/boot.tsx
+++ src/cms.arragro.com/ReactApp/boot.tsx
@@ -6,13 +6,14 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
-import { ConnectedRouter } from 'react-router-redux'
+import { ConnectedRouter } from 'connected-react-router'
 import { createGenerateClassName, jssPreset } from '@material-ui/core/styles'
 import { JssProvider } from 'react-jss'
 import { create } from 'jss'
 
 import { Redux, utils, configureStore, App } from 'arragrocms-management'
 import * as ComponentExtentionTypes from './componentExtentionTypes'
+import { CssBaseline, MuiThemeProvider, createMuiTheme } from '@material-ui/core'
 
 const initialState = (window as any).initialReduxState as Redux.ReduxModel.StoreState
 const store = configureStore(utils.History, true, initialState)
@@ -23,13 +24,32 @@ ComponentExtentionTypes.extendConfigurationTypeMap()
 const generateClassName = createGenerateClassName()
 const jss = create(jssPreset())
 
+const theme = createMuiTheme({
+    palette: {
+        primary: { main: '#f5f5f5' },
+        secondary: {
+            main: '#00A98F',
+            light: '#e6fffb',
+            contrastText: '#FFF'
+        }
+    },
+    shape: {
+        borderRadius: 0
+    }
+})
+
 const render = (Component: any) => {
     ReactDOM.render(
         <Provider store={store}>
             <IntlProvider>
                 <ConnectedRouter history={utils.History}>
                     <JssProvider jss={jss} generateClassName={generateClassName}>
-                        <Component />
+                        <utils.Aux>
+                            <CssBaseline />
+                            <MuiThemeProvider theme={theme}>
+                                <Component />
+                            </MuiThemeProvider>
+                        </utils.Aux>
                     </JssProvider>
                 </ConnectedRouter>
             </IntlProvider>
