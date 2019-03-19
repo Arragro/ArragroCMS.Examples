4d433a500e805f4032d8357b39d5dc035b71a760
Michael Jewell <michael_jewell@hotmail.co.uk>
2019-03-19 19:58:11 +1300

    Upgrades and release to docker

--- src/build.bat
+++ src/build.bat
@@ -1,9 +1,9 @@
-SET VERSION=v0.0.15-alpha
-docker build -f .\CMS.DockerFile -t docker.arragro.com/cms.arragro.com:%VERSION% . --no-cache
-docker build -f .\WWW.DockerFile -t docker.arragro.com/www.arragro.com:%VERSION% . --no-cache
+SET VERSION=v0.0.16-alpha
+REM docker build -f .\CMS.DockerFile -t docker.arragro.com/cms.arragro.com:%VERSION% . --no-cache
+REM docker build -f .\WWW.DockerFile -t docker.arragro.com/www.arragro.com:%VERSION% . --no-cache
 
 docker login docker.arragro.com
 docker push docker.arragro.com/cms.arragro.com:%VERSION%
 docker push docker.arragro.com/www.arragro.com:%VERSION%
 
-docker-compose up -d
+REM docker-compose up -d
