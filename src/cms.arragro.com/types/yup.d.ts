baf6f1380123c906983304d1f61400dc592726c4
Mike Jewell <mike@arragro.com>
2019-08-29 12:32:27 +1200

    Updated npm packages

--- src/cms.arragro.com/types/yup.d.ts
+++ src/cms.arragro.com/types/yup.d.ts
@@ -1,14 +1,18 @@
-import { DateSchema } from 'yup'
+import 'yup'
 
 declare module 'yup' {
     interface StringSchema {
-        numberTest (required: boolean, positive: boolean): StringSchema
-        decimalTest (required: boolean, positive: boolean): StringSchema
-        moreThanCustom (ref: any, msg: any): StringSchema
-        lessThanCustom (ref: any, msg: any): StringSchema
+        numberTest(required: boolean, positive: boolean): StringSchema
+        decimalTest(required: boolean, positive: boolean): StringSchema
+        moreThanCustom(ref: any, msg: any): StringSchema
+        lessThanCustom(ref: any, msg: any): StringSchema
+        equalTo(ref: any, msg: any): StringSchema
     }
     interface DateSchema {
-        moreThanCustom (ref: any, msg: any): DateSchema
-        lessThanCustom (ref: any, msg: any): DateSchema
+        moreThanCustom(ref: any, msg: any): DateSchema
+        lessThanCustom(ref: any, msg: any): DateSchema
+    }
+    interface ObjectSchema<T> {
+        uniqueProperty(property: string, message: string, item: Array<any>, caseSensitive?: boolean): ObjectSchema<T>
     }
 }
