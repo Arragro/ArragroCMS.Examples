6997571d1e92fc7e5c818c12fabafd7c10a45e3c
Mike Jewell <mike@arragro.com>
2019-10-17 10:28:34 +1300

    Removed references to description and keywords

--- src/www.arragro.com/Models/Metadata.cs
+++ src/www.arragro.com/Models/Metadata.cs
@@ -19,23 +19,23 @@ namespace www.arragro.com.Models
         public MetaData(ContentDtoBase content) : this("Arragro", "https://arragro.com")
         {
             var title = string.IsNullOrEmpty(content.Title) ? "Arragro Software Developement for the Cloud" : $"{content.Title} - Arragro";
-            var description = string.IsNullOrEmpty(content.Description) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Description;
-            var keywords = string.IsNullOrEmpty(content.Keywords) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Keywords;
+            //var description = string.IsNullOrEmpty(content.Description) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Description;
+            //var keywords = string.IsNullOrEmpty(content.Keywords) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Keywords;
 
             Title = title;
-            Description = description;
-            Keywords = keywords;
+            //Description = description;
+            //Keywords = keywords;
         }
 
         public MetaData(string siteName, string url, ContentDtoBase content) : this(siteName, url)
         {
             var title = string.IsNullOrEmpty(content.Title) ? "Arragro Software Developement for the Cloud" : $"{content.Title} - Arragro";
-            var description = string.IsNullOrEmpty(content.Description) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Description;
-            var keywords = string.IsNullOrEmpty(content.Keywords) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Keywords;
+            //var description = string.IsNullOrEmpty(content.Description) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Description;
+            //var keywords = string.IsNullOrEmpty(content.Keywords) ? "Azure Architecture Deployment .Net ASP.Net MVC" : content.Keywords;
 
             Title = title;
-            Description = description;
-            Keywords = keywords;
+            //Description = description;
+            //Keywords = keywords;
         }
 
         public MetaData(string title, string description, string keywords) : this("Arragro", "https://arragro.com")
