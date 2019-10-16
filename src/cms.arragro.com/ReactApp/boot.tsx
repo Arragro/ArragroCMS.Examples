a9ae1bc2441b79a9d45f792ab98689a3186688f1
Mike Jewell <mike@arragro.com>
2019-10-17 10:29:32 +1300

    Removed extentions from boot.tsx

--- src/cms.arragro.com/ReactApp/boot.tsx
+++ src/cms.arragro.com/ReactApp/boot.tsx
@@ -7,10 +7,7 @@ import { Provider } from 'react-redux'
 import { IntlProvider } from 'react-intl'
 import { ConnectedRouter } from 'connected-react-router'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider'
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
-import { JssProvider } from 'react-jss'
-import { create } from 'jss'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 async function configureStore () {
     const store = await import('@arragro/cms-management/dist/src/redux/store')
@@ -23,15 +20,12 @@ import App from '@arragro/cms-management/dist/src/asyncApp'
 import { FieldContextProvider } from '@arragro/cms-management/dist/src/components/Field/FieldProvider'
 import { DefaultRenderFieldControlExtender } from '@arragro/cms-management/dist/src/components'
 
-import * as ComponentExtentionTypes from './componentExtentionTypes'
 import { ArragroCMSAdminFieldControlExtender } from './ArragroCMSAdminFieldControlExtender'
 
 const render = async () => {
     const initialState = (window as any).initialReduxState
     const store = (await configureStore())(utils.History, true, initialState)
     const rootEl = document.getElementById('react-app')
-    ComponentExtentionTypes.extendContentTypeMap()
-    ComponentExtentionTypes.extendConfigurationTypeMap()
 
     const theme = createMuiTheme({
         palette: {
